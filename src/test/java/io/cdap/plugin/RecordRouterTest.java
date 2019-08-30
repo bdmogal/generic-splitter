a20725d815b863d73d26529e4264b8233f136ef9
Bhooshan Mogal <bhooshan@google.com>
2019-08-30 14:47:52 -0700

    Added test for routing

--- src/test/java/io/cdap/plugin/RecordRouterTest.java
+++ src/test/java/io/cdap/plugin/RecordRouterTest.java
@@ -32,9 +32,9 @@ import javax.annotation.Nullable;
 public class RecordRouterTest {
   private static final Schema INPUT =
     Schema.recordOf("input",
-                    Schema.Field.of("routingField", Schema.nullableOf(Schema.of(Schema.Type.STRING))),
-                    Schema.Field.of("b", Schema.of(Schema.Type.STRING)),
-                    Schema.Field.of("c", Schema.of(Schema.Type.STRING)));
+                    Schema.Field.of("supplier_id", Schema.nullableOf(Schema.of(Schema.Type.STRING))),
+                    Schema.Field.of("part_id", Schema.of(Schema.Type.STRING)),
+                    Schema.Field.of("count", Schema.of(Schema.Type.INT)));
   private static final String PORT_SPECIFICATION = "a_port:equals(10)";
 
   @Test
@@ -77,6 +77,44 @@ public class RecordRouterTest {
     testDefaultedRecord("skip", null);
   }
 
+  @Test
+  public void testRouting() throws Exception {
+    StructuredRecord testRecord1 = StructuredRecord.builder(INPUT)
+      .set("supplier_id", "supplier1")
+      .set("part_id", "1")
+      .set("count", "10")
+      .build();
+
+    StructuredRecord testRecord2 = StructuredRecord.builder(INPUT)
+      .set("supplier_id", "supplier2")
+      .set("part_id", "2")
+      .set("count", "20")
+      .build();
+
+    StructuredRecord testRecord3 = StructuredRecord.builder(INPUT)
+      .set("supplier_id", "supplier3")
+      .set("part_id", "3")
+      .set("count", "30")
+      .build();
+
+    String portSpecification = "Supplier 1:equals(supplier1),Supplier 2:equals(supplier2),Supplier 3:equals(supplier3)";
+    RecordRouter.Config config = new RecordRouter.Config("supplier_id", portSpecification,
+                                                         "Send to default port", "Default Port",
+                                                         "Send to null port", "Null Port");
+    SplitterTransform<StructuredRecord, StructuredRecord> recordRouter = new RecordRouter(config);
+    recordRouter.initialize(null);
+
+    MockMultiOutputEmitter<StructuredRecord> emitter = new MockMultiOutputEmitter<>();
+
+    recordRouter.transform(testRecord1, emitter);
+    recordRouter.transform(testRecord2, emitter);
+    recordRouter.transform(testRecord3, emitter);
+
+    Assert.assertEquals(testRecord1, emitter.getEmitted().get("Supplier 1").get(0));
+    Assert.assertEquals(testRecord2, emitter.getEmitted().get("Supplier 2").get(0));
+    Assert.assertEquals(testRecord3, emitter.getEmitted().get("Supplier 3").get(0));
+  }
+
   private void testDefaultedRecordToDefaultPort(@Nullable String defaultPortName) throws Exception {
     testDefaultedRecord("Send to default port", defaultPortName);
   }
@@ -87,12 +125,12 @@ public class RecordRouterTest {
 
   private void testDefaultedRecord(String defaultHandling, @Nullable String outputPortName) throws Exception {
     StructuredRecord testRecord = StructuredRecord.builder(INPUT)
-      .set("routingField", "1")
-      .set("b", "2")
-      .set("c", "3")
+      .set("supplier_id", "1")
+      .set("part_id", "2")
+      .set("count", "3")
       .build();
 
-    RecordRouter.Config config = new RecordRouter.Config("routingField", PORT_SPECIFICATION,
+    RecordRouter.Config config = new RecordRouter.Config("supplier_id", PORT_SPECIFICATION,
                                                          defaultHandling, outputPortName, null, null);
     SplitterTransform<StructuredRecord, StructuredRecord> recordRouter = new RecordRouter(config);
     recordRouter.initialize(null);
@@ -112,19 +150,19 @@ public class RecordRouterTest {
       }
       record = (StructuredRecord) objects.get(0);
     }
-    Assert.assertEquals("1", record.get("routingField"));
-    Assert.assertEquals("2", record.get("b"));
-    Assert.assertEquals("3", record.get("c"));
+    Assert.assertEquals("1", record.get("supplier_id"));
+    Assert.assertEquals("2", record.get("part_id"));
+    Assert.assertEquals("3", record.get("count"));
   }
 
   private void testNullRecord(String nullHandling, @Nullable String outputPortName) throws Exception {
     StructuredRecord testRecord = StructuredRecord.builder(INPUT)
-      .set("routingField", null)
-      .set("b", "2")
-      .set("c", "3")
+      .set("supplier_id", null)
+      .set("part_id", "2")
+      .set("count", "3")
       .build();
 
-    RecordRouter.Config config = new RecordRouter.Config("routingField", PORT_SPECIFICATION,
+    RecordRouter.Config config = new RecordRouter.Config("supplier_id", PORT_SPECIFICATION,
                                                          null, null, nullHandling, outputPortName);
     SplitterTransform<StructuredRecord, StructuredRecord> recordRouter = new RecordRouter(config);
     recordRouter.initialize(null);
@@ -144,8 +182,8 @@ public class RecordRouterTest {
       }
       record = (StructuredRecord) objects.get(0);
     }
-    Assert.assertNull(record.get("routingField"));
-    Assert.assertEquals("2", record.get("b"));
-    Assert.assertEquals("3", record.get("c"));
+    Assert.assertNull(record.get("supplier_id"));
+    Assert.assertEquals("2", record.get("part_id"));
+    Assert.assertEquals("3", record.get("count"));
   }
 }
